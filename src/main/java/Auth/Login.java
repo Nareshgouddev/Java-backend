c333ae4739d39fca90890cf1ce771840524f55f6
Naresh goud <ediganareshgoud406@gmail.com>
2026-04-06 22:20:12 +0530

    Authentication

--- src/main/java/Auth/Login.java
+++ src/main/java/Auth/Login.java
@@ -6,49 +6,50 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 
-public class Login extends HttpServlet{
-	protected void doPost(HttpServletRequest req,HttpServletResponse resp)throws ServletException,IOException{
-		String user=req.getParameter("email");
-		String pass=req.getParameter("password");
-	
-		resp.setContentType("text/html");
-		PrintWriter	p=resp.getWriter();
-		int rows=0;
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			String url="jdbc:mysql://localhost:3306/BlogSite";
-			String name="root";
-			String pass1="Naresh@034";
-			Connection c=DriverManager.getConnection(url,name,pass1);
-			
-			
-			String query="INSERT INTO Login(Name,Password) values(?,?)";
-			
-			PreparedStatement ps=c.prepareStatement(query);
-			
-		    ps.setString(1, user);
-		    ps.setString(2, pass);
-		    ps.executeUpdate();
-			
-		    if(rows > 0) {
-		        p.print("<h1>Data saved successfully!</h1>");
-		    } else {
-		        p.print("<h1>Failed to save data.</h1>");
-		    }
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		
-		
-		
-		
-		
-	}
-}
+public class Login extends HttpServlet {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String user = req.getParameter("email");
+        String pass = req.getParameter("password");
 
+        resp.setContentType("text/html");
+        PrintWriter p = resp.getWriter();
+        int row=0;
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String url = "jdbc:mysql://localhost:3306/BlogSite";
+            String name = "root";
+            String pass1 = "Naresh@034";
+            Connection c = DriverManager.getConnection(url, name, pass1);
+
+            String query = "INSERT INTO Login(Name, password) values(?,?)";
+            PreparedStatement ps = c.prepareStatement(query);
+
+            ps.setString(1, user);
+            ps.setString(2, pass);
+            
+            System.out.println(user);
+            System.out.println(pass);
+            
+            row=ps.executeUpdate(); 
+
+            if(row>0) {
+            resp.sendRedirect("Index.html");
+            }
+            else {
+            	p.print("<h1>Insertion failed</h1>");
+            }
+            
+            System.out.println(row);
+            ps.close();
+            c.close();
+
+        } catch (ClassNotFoundException | SQLException e) {
+            p.print("<h1>Error: " + e.getMessage() + "</h1>");
+            e.printStackTrace();
+        }
+    }
+}
